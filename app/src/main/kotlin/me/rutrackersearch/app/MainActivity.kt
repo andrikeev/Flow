2ab4dacbb4566967bcb5391f5f526db7285a91de
Valeriy Andrikeev <v.andrikeev@gmail.com>
2023-03-13 01:26:36 +0300

    Support new splash screen (#2)

--- app/src/main/kotlin/me/rutrackersearch/app/MainActivity.kt
+++ app/src/main/kotlin/me/rutrackersearch/app/MainActivity.kt
@@ -7,18 +7,27 @@ import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat.setDecorFitsSystemWindows
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import flow.designsystem.platform.LocalPlatformType
 import flow.designsystem.platform.PlatformType
+import flow.models.settings.Theme
 import flow.ui.platform.DeeplinkHandler
 import flow.ui.platform.LocalOpenFileHandler
 import flow.ui.platform.LocalOpenLinkHandler
@@ -26,7 +35,11 @@ import flow.ui.platform.LocalShareLinkHandler
 import flow.ui.platform.OpenFileHandler
 import flow.ui.platform.OpenLinkHandler
 import flow.ui.platform.ShareLinkHandler
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import me.rutrackersearch.app.main.MainScreen
+import me.rutrackersearch.app.main.MainViewModel
 import me.rutrackersearch.app.navigation.Navigation
 import me.rutrackersearch.app.platform.DeeplinkHandlerImpl
 import me.rutrackersearch.app.platform.OpenFileHandlerImpl
@@ -37,6 +50,8 @@ import me.rutrackersearch.app.platform.ShareLinkHandlerImpl
 open class MainActivity : ComponentActivity() {
 
     private lateinit var newIntentListener: (Intent) -> Unit
+
+    private val viewModel: MainViewModel by viewModels()
 
     open val deviceType: PlatformType
         get() {
@@ -50,27 +65,46 @@ open class MainActivity : ComponentActivity() {
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+
         setDecorFitsSystemWindows(window, false)
         window.statusBarColor = Color.TRANSPARENT
         window.navigationBarColor = Color.TRANSPARENT
+
         super.onCreate(savedInstanceState)
+
+        var theme: Theme? by mutableStateOf(null)
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.theme
+                    .onEach { theme = it }
+                    .collect()
+            }
+        }
+        splashScreen.setKeepOnScreenCondition { theme == null }
+
         setContent {
-            val navController = rememberNavController()
-            val deeplinkHandler = rememberDeeplinkHandler(navController)
-            val linkHandler = rememberOpenLinkHandler(deeplinkHandler)
-            val shareLinkHandler = rememberShareLinkHandler()
-            val openFileHandler = rememberOpenFileHandler()
-            CompositionLocalProvider(
-                LocalOpenLinkHandler provides linkHandler,
-                LocalShareLinkHandler provides shareLinkHandler,
-                LocalOpenFileHandler provides openFileHandler,
-                LocalPlatformType provides deviceType,
-            ) {
-                MainScreen {
-                    Navigation(navController)
+            theme?.let { theme ->
+                val navController = rememberNavController()
+                val deeplinkHandler = rememberDeeplinkHandler(navController)
+                val linkHandler = rememberOpenLinkHandler(deeplinkHandler)
+                val shareLinkHandler = rememberShareLinkHandler()
+                val openFileHandler = rememberOpenFileHandler()
+                CompositionLocalProvider(
+                    LocalOpenLinkHandler provides linkHandler,
+                    LocalShareLinkHandler provides shareLinkHandler,
+                    LocalOpenFileHandler provides openFileHandler,
+                    LocalPlatformType provides deviceType,
+                ) {
+                    MainScreen(
+                        theme = theme,
+                        platformType = deviceType,
+                        content = { Navigation(navController) },
+                    )
                 }
             }
         }
+
         addOnNewIntentListener { intent ->
             newIntentListener.invoke(intent)
         }
