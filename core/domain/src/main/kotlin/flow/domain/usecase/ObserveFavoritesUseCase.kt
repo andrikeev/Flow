bfd8341c3e2bd13137154371f7b19095d5c20f80
Valeriy Andrikeev <vaandrikeev@sberbank.ru>
2023-05-12 20:59:55 +0300

    Refactor favorites

--- core/domain/src/main/kotlin/flow/domain/usecase/ObserveFavoritesUseCase.kt
+++ core/domain/src/main/kotlin/flow/domain/usecase/ObserveFavoritesUseCase.kt
@@ -4,10 +4,17 @@ import flow.data.api.repository.FavoritesRepository
 import flow.models.topic.Topic
 import flow.models.topic.TopicModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 class ObserveFavoritesUseCase @Inject constructor(
     private val favoritesRepository: FavoritesRepository,
+    private val refreshFavoritesUseCase: RefreshFavoritesUseCase,
 ) {
-    operator fun invoke(): Flow<List<TopicModel<out Topic>>> = favoritesRepository.observeTopics()
+    operator fun invoke(): Flow<List<TopicModel<out Topic>>> {
+        return favoritesRepository.observeTopics()
+            .onStart { refreshFavoritesUseCase() }
+            .catch { favoritesRepository.clear() }
+    }
 }
--- feature/visited/src/main/kotlin/flow/visited/VisitedViewModel.kt
+++ feature/visited/src/main/kotlin/flow/visited/VisitedViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import flow.domain.usecase.ObserveVisitedUseCase
 import flow.domain.usecase.ToggleFavoriteUseCase
+import flow.logger.api.LoggerFactory
 import flow.models.topic.Topic
 import flow.models.topic.TopicModel
 import kotlinx.coroutines.flow.collectLatest
@@ -19,13 +20,17 @@ import javax.inject.Inject
 internal class VisitedViewModel @Inject constructor(
     private val observeVisitedUseCase: ObserveVisitedUseCase,
     private val toggleFavoriteUseCase: ToggleFavoriteUseCase,
+    loggerFactory: LoggerFactory,
 ) : ViewModel(), ContainerHost<VisitedState, VisitedSideEffect> {
+    private val logger = loggerFactory.get("VisitedViewModel")
+
     override val container: Container<VisitedState, VisitedSideEffect> = container(
         initialState = VisitedState.Initial,
         onCreate = { observeVisited() },
     )
 
     fun perform(action: VisitedAction) {
+        logger.d { "Perform $action" }
         when (action) {
             is VisitedAction.FavoriteClick -> onFavoriteClick(action.topicModel)
             is VisitedAction.TopicClick -> onTopicClick(action.topicModel)
@@ -33,7 +38,9 @@ internal class VisitedViewModel @Inject constructor(
     }
 
     private fun observeVisited() = intent {
+        logger.d { "Start observing visited" }
         observeVisitedUseCase().collectLatest { items ->
+            logger.d { "On new visited list: $items" }
             reduce {
                 if (items.isEmpty()) {
                     VisitedState.Empty
